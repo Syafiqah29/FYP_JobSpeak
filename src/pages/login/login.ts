cb950dbe1f688f45ebfbcfd9e539c609c36a341c
Pikaa <nws05_01082270@pb.edu.bn>
2019-08-14 13:48:13 +0800

    login td

---
+++ src/pages/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'home.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
