40286ea55afda0fa119e05e93bd63062bbe3d80b
Ainaa Sueini <nws04_01088146@pb.edu.bn>
2019-08-21 11:27:16 +0800

    firebase.credentials

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCuLm09gF9BEYVd8_lYlDVOXXLyBQh0A2w",
+    authDomain: "job-speak-59adc.firebaseapp.com",
+    databaseURL: "https://job-speak-59adc.firebaseio.com",
+    projectId: "job-speak-59adc",
+    storageBucket: "job-speak-59adc.appspot.com",
+    messagingSenderId: "647408446062",
+    appId: "1:647408446062:web:11a615e2085f7ef4"
+  };
