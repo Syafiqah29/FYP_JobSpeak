df493f7b58b22def9501f0a3732adba218fdb167
Ainaa Sueini <nws04_01088146@pb.edu.bn>
2019-08-21 12:47:00 +0800

    edit firebase credentials

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,9 +1,9 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCuLm09gF9BEYVd8_lYlDVOXXLyBQh0A2w",
-    authDomain: "job-speak-59adc.firebaseapp.com",
-    databaseURL: "https://job-speak-59adc.firebaseio.com",
-    projectId: "job-speak-59adc",
-    storageBucket: "job-speak-59adc.appspot.com",
-    messagingSenderId: "647408446062",
-    appId: "1:647408446062:web:11a615e2085f7ef4"
+  apiKey: "AIzaSyCxuQ8uROuUrMVHCj2i0RwDjUwlpo6VvVs",
+  authDomain: "job-speak-5e1c6.firebaseapp.com",
+  databaseURL: "https://job-speak-5e1c6.firebaseio.com",
+  projectId: "job-speak-5e1c6",
+  storageBucket: "job-speak-5e1c6.appspot.com",
+  messagingSenderId: "347804093851",
+  appId: "1:347804093851:web:7be1868ee9f70a94"
   };
