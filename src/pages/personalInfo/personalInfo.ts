031a9678459219327ccca38bf67ae71ef9c77f6a
Pikaa <nws05_01082270@pb.edu.bn>
2019-08-28 13:33:19 +0800

    updated

---
+++ src/pages/personalInfo/personalInfo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-personalInfo',
+  templateUrl: 'personalInfo.html'
+})
+export class SplashscreenPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
