fa96c258b6eab4fbeaf01a108beb6b3cfb2e22f2
Ainaa Sueini <nws04_01088146@pb.edu.bn>
2019-09-09 14:03:11 +0800

    firestore personal info and edu

---
+++ src/models/education.ts
@@ -0,0 +1,29 @@
+export interface Education {
+    key?: string;
+
+    Oschool: string;
+    Oyear: number;
+
+    Aschool: string;
+    Ayear: number;
+
+    Nschool: string;
+    Nyear: number;
+    Ncourse: string;
+
+    Hschool: string;
+    Hyear: number;
+    Hcourse: string;
+
+    Dschool: string;
+    Dyear: number;
+    Dcourse: string;
+
+    Mschool: string;
+    Myear: number;
+    Mcourse: string;
+
+    Pschool: string;
+    Pyear: number;
+    Pcourse: string;
+}
---
+++ src/models/personal.info.ts
@@ -0,0 +1,51 @@
+export interface PersonalInfo {
+    key?: string;
+    
+    name: string;
+    icnumber: number;
+    DOB: string;
+    
+    male: string;
+    female: string;
+    
+    muslim: string;
+    christian: string;
+    buddhist: string;
+    other: string;
+
+    single: string;
+    married: string;
+    divorced: string;
+    widowed: string;
+
+    malay: string;
+    kedayan: string;
+    dusun: string;
+    murut: string;
+
+    address: string;
+    phone: number;
+
+    DLyes: string;
+    DLno: string;
+    DL1: string;
+    DL2: string;
+    DL3: string;
+    DL4: string;
+    DL5: string;
+    DL6: string;
+    DL7: string;
+    DL8: string;
+    DL9: string;
+    DL10: string;
+    DL11: string;
+    DL12: string;
+
+    parents: string;
+    guardian: string;
+    spouse: string;
+
+    Fname: string;
+    Fic: string;
+    Fphone: string;
+} 
