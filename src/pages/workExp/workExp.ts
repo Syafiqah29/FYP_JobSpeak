fde471e1349d894a65438ec65822084e28229018
Syafiqah29 <nws05_01088270@pb.edu.bn>
2019-09-13 22:35:02 +0800

    validations updated

--- src/pages/workExp/workExp.ts
+++ src/pages/workExp/workExp.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { LoginPage } from '../login/login';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'page-workExp',
@@ -8,8 +9,30 @@ import { LoginPage } from '../login/login';
 })
 export class WorkExperiencePage {
 
-  constructor(public navCtrl: NavController) {
+  workForm: FormGroup;
+  organizationName: AbstractControl;
+  titlePost: AbstractControl;
+  reasonLeaving: AbstractControl;
+  refereeName: AbstractControl;
+  refereeNumber: AbstractControl;
 
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private formbuilder: FormBuilder) {
+      
+      this.workForm = formbuilder.group({
+        organizationName: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]*$')])],
+        titlePost: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]*$')])],
+        reasonLeaving: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]*$')])],
+        refereeName: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z\'@ ]+')])],
+        refereeNumber: ['', Validators.compose([Validators.required, Validators.maxLength(7)])],
+      });
+
+      this.organizationName = this.workForm.controls['organizationName'];
+      this.titlePost = this.workForm.controls['titlePost'];
+      this.reasonLeaving = this.workForm.controls['reasonLeaving'];
+      this.refereeName = this.workForm.controls['refereeName'];
+      this.refereeNumber = this.workForm.controls['refereeNumber'];
   }
   gotoLogin(){
     this.navCtrl.push(LoginPage);
