cb8eace51693822818325bccf4e2450f9dc4b9fb
wardahOmar <nws04_01083200@pb.edu.bn>
2019-09-14 11:24:17 +0800

    model

---
+++ src/models/workExperience.model.ts
@@ -0,0 +1,15 @@
+export interface WorkExperience {
+    key?: string;
+
+    organization : string;
+    post : string;
+    WorkYear: number;
+    WorkYear2: number;
+    reason: string;
+    referee: string;
+    RefereeNumber : number;
+    skills: string;
+    
+
+
+}
