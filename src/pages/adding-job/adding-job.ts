e805bb0af08ae66b3f9244792151336eec5d6a92
wardahOmar <nws04_01083200@pb.edu.bn>
2019-09-30 19:52:36 +0800

    ts

--- src/pages/adding-job/adding-job.ts
+++ src/pages/adding-job/adding-job.ts
@@ -3,7 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { addingJob } from '../../models/addingJob.model';
 import { AdminDashboardPage } from '../admin-dashboard/admin-dashboard';
 import { AngularFireAuth } from 'angularfire2/auth';
-import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import { FormGroup } from '@angular/forms';
+import { database } from 'firebase';
+import { Observable } from 'rxjs';
 
 /**
  * Generated class for the AddingJobPage page.
@@ -19,11 +22,16 @@ import {AngularFireDatabase} from 'angularfire2/database';
 })
 export class AddingJobPage {
 
-  addJob = {} as addingJob;
+
+  addingjob = {} as addingJob;
+
+  
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams,private afDatabase: AngularFireDatabase
     ,private afAuth: AngularFireAuth) {
   }
+
 
  
 
@@ -34,13 +42,49 @@ export class AddingJobPage {
   addjob(){
   this.navCtrl.push(AdminDashboardPage);
     this.afAuth.authState.take(1).subscribe(auth => {
-      this.afDatabase.object(`job details/${auth.uid}`).set(this.addJob)
+      this.afDatabase.object(`job details/${auth.uid}`).set(this.addingjob)
        .then(() => this.navCtrl.setRoot(AdminDashboardPage));
    })
 
   }
 
-   ionViewDidLoad() {
-    console.log('ionViewDidLoad AddingJobPage');
-  }
+
+
 }
+
+// const jobform = document.querySelector ('#jobform');
+// const db = database;
+
+
+// function addjob(doc) {
+ 
+//   let li = document.createElement('li');
+//   let jobname = document.createElement('jobname');
+//   let requirements = document.createElement('span');
+//   let descriptions = document.createElement('span');
+//   let salary = document.createElement('span');
+//   let availability = document.createElement('span');
+
+//   li.setAttribute('data-id', doc.id);
+//   jobname.textContent = doc.data().jobname;
+//   requirements.textContent = doc.data().requirements;
+//   descriptions.textContent = doc.data().descriptions;
+//   salary.textContent = doc.data().salary;
+//   availability.textContent = doc.data().availability;
+
+//   li.appendChild (jobname);
+//   li.appendChild (requirements);
+//   li.appendChild (descriptions);
+//   li.appendChild (salary);
+//   li.appendChild (availability);
+
+//   jobform.appendChild(li);
+  
+
+
+// }
+// this.db.collection('job details').get().then((snapshot) =>{
+//   snapshot.docs.forEach(doc =>{
+//     addjob(doc);
+//   })
+// })
