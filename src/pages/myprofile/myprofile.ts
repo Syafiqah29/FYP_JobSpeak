d32b6abacb490285c9e1d44702c0dbb1e691c638
Syafiqah29 <nws05_01088270@pb.edu.bn>
2019-10-01 19:31:20 +0800

    retrieve

--- src/pages/myprofile/myprofile.ts
+++ src/pages/myprofile/myprofile.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { AngularFireList, AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { PersonalInfo } from '../../models/personalInfo.model';
+import { PersonalInformationPage } from '../personalInfo/personalInfo';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map'; 
+//import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'page-myprofile',
   templateUrl: 'myprofile.html'
 })
-export class MyprofilePage {
+export class MyprofilePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  private personalInfo: Observable<PersonalInfo[]>;
 
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private afAuth: AngularFireAuth,
+    private afDatabase: AngularFireDatabase) {
   }
+ 
+  ngOnInit(){
+    this.personalInfo = this.afDatabase.list<PersonalInfo>('personalInfo').snapshotChanges().map(changes => {
+      return changes.map(c => ({
+        key: c.payload.key,
+        ...c.payload.val()
+      }));
+    });
+  }
+
+  // ionViewDidLoad(){
+  //   this.afAuth.authState.subscribe(data => {
+  //     if (data && data.email && data.uid){
+  //       this.personalInfoRef$ = this.afDatabase.list(`personal/${data.uid}`);
+  //     }
+  //   })
+  // }
+  
+  // ionViewDidLoad(){
+  //   this.afAuth.authState.subscribe(data => {
+  //     console.log('personalInfo' + data.uid);
+  //     return data.uid;
+  //   });
+  // }
+
 
 }
