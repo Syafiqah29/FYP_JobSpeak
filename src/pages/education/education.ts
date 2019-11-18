5a88ed0f50b8abcf77dc419acd62efac9d0ed39d
ummizainal <nws04_01083215@pb.edu.bn>
2019-11-18 18:35:59 +0800

    added capture and upload btn with function

--- src/pages/education/education.ts
+++ src/pages/education/education.ts
@@ -7,7 +7,7 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { HttpClient } from '@angular/common/http';
 import { storage, initializeApp } from 'firebase'
 import { FIREBASE_CONFIG } from '../../app/app.firebase.config';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-education',
@@ -17,6 +17,20 @@ export class EducationPage {
 
   education = {} as Education;
 
+  // TRYOUT 18/11
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    cameraDirection:0
+  }
+
+  OImage: any;
+  HImage: any;
+  DImage: any;
+  // TRYOUT 18/11
+
   constructor(public navCtrl: NavController,
     private camera: Camera,
     private afAuth: AngularFireAuth, 
@@ -25,187 +39,133 @@ export class EducationPage {
 
   }
 
-  async OtakePhoto(){
-    try{
-    const options: CameraOptions = {
-      quality: 50,
-      targetHeight: 600,
-      targetWidth: 600,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE,
-      correctOrientation: true
-    }
+  // TRYOUT 18/11
+  // O Level
+  OclickImage(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64ImageO = 'data:image/jpeg;base64,' + imageData;
+      this.OImage = 'data:image/jpeg;base64,' + imageData;
+
+      const pictures = storage().ref('OLevel/photo');
+      pictures.putString(base64ImageO, 'data_url');
+
+     }, (err) => {
+      // Handle error
+     });
+  }
+
+  // HND
+  HclickImage(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64ImageH = 'data:image/jpeg;base64,' + imageData;
+      this.HImage = 'data:image/jpeg;base64,' + imageData;
+    
+      const pictures = storage().ref('HND/photo');
+      pictures.putString(base64ImageH, 'data_url');
+
+     }, (err) => {
+      // Handle error
+     });
+  }
+
+  // Degree
+  DclickImage(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64ImageD = 'data:image/jpeg;base64,' + imageData;
+      this.DImage = 'data:image/jpeg;base64,' + imageData;
+      
+      const pictures = storage().ref('Degree/photo');
+      pictures.putString(base64ImageD, 'data_url');
+
+     }, (err) => {
+      // Handle error
+     });
+  }
+  // TRYOUT 18/11
+
+//   async OtakePhoto(){
+//     try{
+//     const options: CameraOptions = {
+//       quality: 50,
+//       targetHeight: 600,
+//       targetWidth: 600,
+//       destinationType: this.camera.DestinationType.DATA_URL,
+//       encodingType: this.camera.EncodingType.JPEG,
+//       mediaType: this.camera.MediaType.PICTURE,
+//       correctOrientation: true
+//     }
   
-    const result = await this.camera.getPicture(options);
+//     const result = await this.camera.getPicture(options);
 
-    const image = `data:image/jpeg;base64,${result}`;
+//     const image = `data:image/jpeg;base64,${result}`;
 
-    const pictures = storage().ref('OLevel/pictures');
-    pictures.putString(image, 'data_url');
-
-
-  }
-  catch (e){
-    console.error(e);
-  }
-}
-
-async AtakePhoto(){
-  try{
-  const options: CameraOptions = {
-    quality: 50,
-    targetHeight: 600,
-    targetWidth: 600,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    correctOrientation: true
-  }
-
-  const result = await this.camera.getPicture(options);
-
-  const image = `data:image/jpeg;base64,${result}`;
-
-  const pictures = storage().ref('ALevel/pictures');
-  pictures.putString(image, 'data_url');
+//     const pictures = storage().ref('OLevel/pictures');
+//     pictures.putString(image, 'data_url');
 
 
-}
-catch (e){
-  console.error(e);
-}
-}
+//   }
+//   catch (e){
+//     console.error(e);
+//   }
+// }
 
-async NtakePhoto(){
-  try{
-  const options: CameraOptions = {
-    quality: 50,
-    targetHeight: 600,
-    targetWidth: 600,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    correctOrientation: true
-  }
+// async HtakePhoto(){
+//   try{
+//   const options: CameraOptions = {
+//     quality: 50,
+//     targetHeight: 600,
+//     targetWidth: 600,
+//     destinationType: this.camera.DestinationType.DATA_URL,
+//     encodingType: this.camera.EncodingType.JPEG,
+//     mediaType: this.camera.MediaType.PICTURE,
+//     correctOrientation: true
+//   }
 
-  const result = await this.camera.getPicture(options);
+//   const result = await this.camera.getPicture(options);
 
-  const image = `data:image/jpeg;base64,${result}`;
+//   const image = `data:image/jpeg;base64,${result}`;
 
-  const pictures = storage().ref('ND/pictures');
-  pictures.putString(image, 'data_url');
-
-
-}
-catch (e){
-  console.error(e);
-}
-}
-
-async HtakePhoto(){
-  try{
-  const options: CameraOptions = {
-    quality: 50,
-    targetHeight: 600,
-    targetWidth: 600,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    correctOrientation: true
-  }
-
-  const result = await this.camera.getPicture(options);
-
-  const image = `data:image/jpeg;base64,${result}`;
-
-  const pictures = storage().ref('HND/pictures');
-  pictures.putString(image, 'data_url');
+//   const pictures = storage().ref('HND/pictures');
+//   pictures.putString(image, 'data_url');
 
 
-}
-catch (e){
-  console.error(e);
-}
-}
+// }
+// catch (e){
+//   console.error(e);
+// }
+// }
 
-async DtakePhoto(){
-  try{
-  const options: CameraOptions = {
-    quality: 50,
-    targetHeight: 600,
-    targetWidth: 600,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    correctOrientation: true
-  }
+// async DtakePhoto(){
+//   try{
+//   const options: CameraOptions = {
+//     quality: 50,
+//     targetHeight: 600,
+//     targetWidth: 600,
+//     destinationType: this.camera.DestinationType.DATA_URL,
+//     encodingType: this.camera.EncodingType.JPEG,
+//     mediaType: this.camera.MediaType.PICTURE,
+//     correctOrientation: true
+//   }
 
-  const result = await this.camera.getPicture(options);
+//   const result = await this.camera.getPicture(options);
 
-  const image = `data:image/jpeg;base64,${result}`;
+//   const image = `data:image/jpeg;base64,${result}`;
 
-  const pictures = storage().ref('Degree/pictures');
-  pictures.putString(image, 'data_url');
-
-
-}
-catch (e){
-  console.error(e);
-}
-}
-
-async MtakePhoto(){
-  try{
-  const options: CameraOptions = {
-    quality: 50,
-    targetHeight: 600,
-    targetWidth: 600,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    correctOrientation: true
-  }
-
-  const result = await this.camera.getPicture(options);
-
-  const image = `data:image/jpeg;base64,${result}`;
-
-  const pictures = storage().ref('Master/pictures');
-  pictures.putString(image, 'data_url');
+//   const pictures = storage().ref('Degree/pictures');
+//   pictures.putString(image, 'data_url');
 
 
-}
-catch (e){
-  console.error(e);
-}
-}
-
-async PtakePhoto(){
-  try{
-  const options: CameraOptions = {
-    quality: 50,
-    targetHeight: 600,
-    targetWidth: 600,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    correctOrientation: true
-  }
-
-  const result = await this.camera.getPicture(options);
-
-  const image = `data:image/jpeg;base64,${result}`;
-
-  const pictures = storage().ref('PhD/pictures');
-  pictures.putString(image, 'data_url');
-
-
-}
-catch (e){
-  console.error(e);
-}
-}
+// }
+// catch (e){
+//   console.error(e);
+// }
+// }
 
   gotoWork(education: Education){
     this.navCtrl.push(WorkExperiencePage);
